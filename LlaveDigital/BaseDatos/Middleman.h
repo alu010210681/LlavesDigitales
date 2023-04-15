2c8e47df7cd2d68f3b537a93be4c9843bc2be477
Samuel Lorenzo Sánchez <48592338+alu010210681@users.noreply.github.com>
2023-04-15 16:09:46 +0100

    Middleware Constructors

--- LlaveDigital/BaseDatos/Middleman.h
+++ LlaveDigital/BaseDatos/Middleman.h
@@ -7,59 +7,53 @@ template <class InfoType>
 class Middleman {
  public:
   // constructores 
-  Middleman();
-  Middleman(string); // nombre del archivo
-
+  Middleman(string, string = ""); // nombre del archivo
   // ----------------- Metodos BASICOS
   /*
    cambiar de archivo
-  */
-  bool change_file(string); 
+  /*/
+  //bool change_file(string); 
   /*
     abrir el archivo con sus permisos/restricciones
-
-    RESTRICCIONES:
+  */
+  bool open(); // se abre en formato escritura y lectura
+  /*RESTRICCIONES:
       r -> Read
       w -> Write
       e -> End
-      t -> Truncate
-  */
-
-
-  bool open(); // se abre en formato escritura y lectura
+      t -> Truncate*/
   bool open(string); // "Formato -> rw o r"
-  bool open(bool, bool, bool, bool); // 1,0 = r y !w  
+  bool open(bool, bool, bool, bool); // 1,0 = r y !w 
   /*
     cerrar el archivo 
   */
-  bool close(); 
+  //bool close(); 
   /*
     Vuelve a cargar el archivo en el programa
   */
-  bool refresh(); 
+  //bool refresh(); 
   // ----------------- Metodos INSERCION
   /*
     Insertar en la posicion del puntero contenplado en el fichero fstream
   */
-  bool insert(InfoType);
+  //bool insert(InfoType);
   /*
     Insertar al inicio del archivo
   */
-  bool insert_front(InfoType);
+  //bool insert_front(InfoType);
   /*
     Insertar al final del archivo
   */
-  bool insert_back(InfoType);
+  //bool insert_back(InfoType);
   /*
     Insertar en una linea en concreto del archivo
   */
-  bool insert_in_line(int, InfoType);
+  //bool insert_in_line(int, InfoType);
   /*
     insertar con la ultima linea del conjunto encontrada que 
     empiece por el tag pasado por parametro
   */
-  bool insert_sorted(string, InfoType); 
-
+  //bool insert_sorted(string, InfoType); 
  private:
   string file_name_;
   fstream file_;
